8eb2f9040a6a0fa1f237e5ce50040053446d0753
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-03-27 08:24:20 +0300

    Add new routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import fruitRoutes from './routes/fruitRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,8 +14,11 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
+// Routes
 app.use('/api/', fruitRoutes);
 app.use('/api/auth/', authRoutes);
+app.use('/api/admin/', adminRoutes);
+app.use('/api/user/', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
